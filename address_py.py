124f58c14ebcef4cb24de2eda1dd2294346f1859
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-01-22 18:25:37 -0800

    cleaned up source code

--- address_py.py
+++ address_py.py
@@ -1,29 +1,38 @@
 #!/usr/bin/python3
 
-from sqlite3 import connect
+import sqlite3
 
-db = connect ('test.db')
+# http://tutorialspoint.com/sqlite/sqlite_python.htm
 
-answer = input("Press (1) to store, (2) to retrieve\n")
-print(answer)
+def main():
+    answer = input("Press (1) to store, (2) to retrieve\n")
+    print(answer)
+    if answer == "1": # enter function to store data into Address Book
+        print("write to database")
+        write_function()
+    if answer == "2":  # access address boook
+        print("Read from database")
+        read_function()
 
-if answer == "1": # enter function to store data into Address Book
-    store_var()
-if answer == "2":  # access address boook
-    write_db()
-
+def write_function():
     try:
-        cursor.execute(sqlite_insert_query(sqlite_insert_query)
-        sqliteConnection.commit()
+        sqliteConnection = sqlite.connect('test.db')
+        cursor = sqliteConnection.cursor()
+        cursor.execute(sqlite_insert_query)
+        sqlteConnection.commit()
         cursor.close()
-    except sqlite3.Error as erorr:
-        print("Erorr while working with SQLite", errorr)
-    finally
+    except sqlite3.Error as error:
+        print("Error while working with SQLite", error)
+    finally:
         if sqliteConnection:
             print("Total Rows affected since the database connection was opened: ")
             sqliteConnection.close()
             print("sqlite connection is closed")
 
-def write_db():
-    
-db.close()
+def read_function(): # read DB
+    try:
+        sqliteConnection = sqlite.connect('test.db') # connect to db
+    except: # if error (db is not read)
+        print("Error reading DB")
+
+main()
