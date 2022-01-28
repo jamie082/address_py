9a2d4a5680e697759a44dfb6895a78e9ac390326
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-01-27 18:45:37 -0800

    updated working source

--- address_py.py
+++ address_py.py
@@ -2,29 +2,29 @@
 
 import sqlite3 
 
+con = sqlite3.connect("test.db")
+
 # http://tutorialspoint.com/sqlite/sqlite_python.htm
 
 def main():
     answer = input("Press (1) to store, (2) to retrieve\n")
     print(answer)
     if answer == "1": # enter function to store data into Address Book
-        print("write to database")
+        #print("write to database")
         write_function()
     if answer == "2":  # access address boook
-        print("Read from database")
+        #print("Read from database")
         read_function()
 
 def write_function():
     try:
-        con = sqlite3.connect("test.db")
         cursor = con.cursor()
         personal_name = input("Name: ")
         address = input("Address Line: ")
-        zip_code = input("Zip Code: ")
+        zip_code = int(input("Zip Code: "))
         phone_number = input("Phone #: ")
-
-        data = (personal_name, address, zip_code)
-        data = ''' (personal_name, address, zip_code, phone_number) VALUES (?,?,?,?) '''
+        data = (personal_name, address, zip_code, phone_number)
+        query = ''' INSERT into HASH (personal_name, address, zip_code, phone_number) VALUES (?,?,?,?) '''
         cursor.execute(query, data)
         con.commit()
         if (cursor.execute(query,data)):
@@ -34,22 +34,28 @@ def write_function():
         cursor.close()
     except:
         print("Database Error")
-    finally:
-        # Something else went wrong
-            print("sqlite connection is closed")
-
-    con.close() # db close
+        con.close() # db close
 
 def read_function(): # read DB
     try:
-        sqliteConnection = sqlite3.connection('test.db')   
-        print("Connected to Sqlite")
-        sql_query = """SELECT name FROM sqlite_master WHERE type='table';"""
-        cursor = sqliteConnection.cursor()
-        print("List of tables\n")
-    except: # if error (db is not read)
-        print("Error reading DB")
+        cursor = con.cursor()
+        select_data = 'SELECT * FROM HASH'
+        cursor.execute(select_data)
 
-    con.close()
+        row = cursor.fetchone()
+        print(row)
+
+        #con.commit()
+        
+        # close the connection
+        #con.close()
+    except:
+        print("Error")
+
+    finally:
+        print("Something went wrong (also)")
+
+
 
 main()
+
